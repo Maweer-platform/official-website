c18d1ef8a82b1769ecc0b6c986bb1b1a1f6a3c72
banahaker <pn0818x@outlook.com>
2022-07-28 22:19:14 +0800

    deploy

--- script/gh-pages.sh
+++ script/gh-pages.sh
@@ -1,12 +1,12 @@
 rm -rf ./public
+git clone git@github.com:Mashar-io/official-website.git public
+
 sass scss:css
 rsync -Rr ./*.html ./public
 rsync -Rr ./css ./public
+rsync -Rr ./javascript/ ./public/
 cd public
-git init
-git remote add origin git@github.com:Mashar-io/official-website.git
-git checkout -b gh-pages
-git pull
+git checkout gh-pages
 git add .
 git commit -m "gh-pages"
 git push origin gh-pages
