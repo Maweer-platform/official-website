9d0497ed941cd870c9950030416b7534ea9510bc
banahaker <pn0818x@outlook.com>
2022-07-29 22:16:20 +0800

    feat: make shell script better

--- script/gh-pages.sh
+++ script/gh-pages.sh
@@ -1,13 +1,17 @@
 rm -rf ./public
 git clone git@github.com:Mashar-io/official-website.git public
 
+cd public
+git checkout gh-pages
+cd ..
+
 sass scss:css
 rsync -Rr ./*.html ./public
 rsync -Rr ./css ./public
 rsync -Rr ./javascript/ ./public/
 rsync -Rr ./assets/ ./public/
 cd public
-git checkout gh-pages
+git status
 git add .
 git commit -m "gh-pages"
 git push origin gh-pages
